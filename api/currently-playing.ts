34ee9544604d99ce3b98da7eae595ab879f378d5
pcktm <jakub@kopanko.com>
2024-02-05 16:46:43 +0100

    feat: vercel edge functions

---
+++ api/currently-playing.ts
@@ -0,0 +1,11 @@
+import type { RequestContext } from '@vercel/edge';
+
+export const config = {
+  runtime: 'edge',
+};
+
+export function GET(request: Request, context: RequestContext) {
+  console.log('Region:', process.env.VERCEL_REGION);
+  console.log('Deployment URL:', process.env.VERCEL_URL);
+  return new Response(`Hello from ${process.env.VERCEL_REGION}, ${process.env.VERCEL_URL}`);
+}
