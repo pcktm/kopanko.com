387f04ea106b5bfe87b66a9c575235a0c508a873
pcktm <jakub@kopanko.com>
2024-02-13 14:21:43 +0100

    feat: projects page

---
+++ src/lib/cms/achievements.ts
@@ -0,0 +1,39 @@
+import type { Achievement } from "./types";
+import { imageToCssPlaceholder, request } from "./utils";
+
+type Props = {
+  locale?: string;
+};
+
+export async function getAchievements(props?: Props): Promise<Achievement[]> {
+  const query = `query getAchievements {
+    achievements(orderBy: priority_ASC, locales: [${props?.locale ?? 'en'}]) {
+      id
+      title
+      tagline
+      action
+      isMinor
+      description {
+        html
+      }
+      coverImage(locales: en) {
+        url
+        width
+        height
+        placeholder: url(
+          transformation: {
+            image: { resize: { width: 10, height: 10, fit: clip } }
+          }
+        )
+      }
+    }
+  }`;
+  const {achievements} = await request<{achievements: Achievement[]}>(query);
+  const achievementsWithPlaceholder = await Promise.all(achievements.map(async (ach) => {
+    if (ach.coverImage?.placeholder) {
+      ach.coverImage.placeholder = await imageToCssPlaceholder(ach.coverImage.placeholder as any);
+    }
+    return ach;
+  }));
+  return achievementsWithPlaceholder;
+}
---
+++ src/lib/cms/projects.ts
@@ -0,0 +1,40 @@
+import type { Project } from "./types";
+import { imageToCssPlaceholder, request } from "./utils";
+
+type Props = {
+  locale?: string;
+};
+
+export async function getProjects(props?: Props): Promise<Project[]> {
+  const query = `query getProjects {
+    projects(orderBy: priority_ASC, locales: [${props?.locale ?? 'en'}]) {
+      tagline
+      title
+      id
+      action
+      isMinor
+      description {
+        html
+      }
+      coverImage(locales: en) {
+        url
+        width
+        height
+        placeholder: url(
+          transformation: {
+            image: { resize: { width: 10, height: 10, fit: clip } }
+          }
+        )
+      }
+    }
+  }`;
+  const {projects} = await request<{projects: Project[]}>(query);
+  const projectsWithPlaceholder = await Promise.all(projects.map(async (project) => {
+    if (project.coverImage?.placeholder) {
+      project.coverImage.placeholder = await imageToCssPlaceholder(project.coverImage.placeholder as any);
+    }
+    return project;
+  }));
+  return projectsWithPlaceholder;
+}
+
