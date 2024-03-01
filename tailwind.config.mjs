ad5d834417855aaaba65696f2b86d1504dcbc315
pcktm <hi@kopanko.com>
2024-03-01 12:43:54 +0100

    feat: dark mode toggle

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,7 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'selector',
 	theme: {
 		extend: {
 			fontFamily: {
