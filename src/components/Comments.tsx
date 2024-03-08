9a3c2e4ccfe5a596175dc2000fc5a72346831ae2
pcktm <hi@kopanko.com>
2024-03-08 14:59:02 +0100

    fix: comments theme

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -6,20 +6,21 @@ type CommentsProps = {
 };
 
 export default function Comments({ term }: CommentsProps) {
-  const [theme, setTheme] = useState(
-    window.matchMedia("(prefers-color-scheme: dark)").matches
-      ? "transparent_dark"
-      : "light",
-  );
-  useEffect(() => {
-    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
-    const listener = (e: MediaQueryListEvent) => {
-      const newTheme = e.matches ? "transparent_dark" : "light";
-      setTheme(newTheme);
-    };
-    mediaQuery.addEventListener("change", listener);
-    return () => mediaQuery.removeEventListener("change", listener);
-  }, []);
+  // const [theme, setTheme] = useState(
+  //   window.matchMedia("(prefers-color-scheme: dark)").matches
+  //     ? "transparent_dark"
+  //     : "light",
+  // );
+  // useEffect(() => {
+  //   const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+  //   const listener = (e: MediaQueryListEvent) => {
+  //     const newTheme = e.matches ? "transparent_dark" : "light";
+  //     setTheme(newTheme);
+  //   };
+  //   mediaQuery.addEventListener("change", listener);
+  //   return () => mediaQuery.removeEventListener("change", listener);
+  // }, []);
+  const theme = "light";
   return (
     <Giscus
       repo="pcktm/kopanko.com"
