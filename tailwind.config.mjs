2adeb29f2081ff07a1b30745ee6822342e97159e
pcktm <hi@kopanko.com>
2024-09-17 20:35:23 +0200

    feat: enable dark mode, add non-profit work section

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,6 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	darkMode: 'selector',
 	theme: {
 		extend: {
 			fontFamily: {
