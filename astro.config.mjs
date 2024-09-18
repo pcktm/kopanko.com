6ebf884b99b8ada92fd884ec3a7ee605af97e5a1
pcktm <hi@kopanko.com>
2024-09-18 21:26:34 +0200

    feat: add skill icons to work experience

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import metaTags from "astro-meta-tags";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), metaTags()],
+  integrations: [tailwind(), react(), metaTags(), icon()],
   image: {
     domains: ["graphassets.com"]
   },
