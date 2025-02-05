37254e41b2999797dcca12463d493deb5d9e0130
Jakub Kopańko <13273420+pcktm@users.noreply.github.com>
2025-02-05 16:34:37 +0000

    feat: upgrade astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import metaTags from "astro-meta-tags";
 import icon from "astro-icon";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), metaTags(), icon()],
+  integrations: [react(), metaTags(), icon()],
+  vite: {
+    plugins: [tailwindcss()]
+  },
   image: {
     domains: ["graphassets.com"],
     remotePatterns: [{
